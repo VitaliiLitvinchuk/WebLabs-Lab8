cb246d064828784ca0814a465da7a4b22c21aec3
Vitalik-Litvinchuk <61794076+Vitalik-Litvinchuk@users.noreply.github.com>
2024-03-20 23:56:27 +0200

    feat(tailwind): add tailwind and nav

--- app.vue
+++ app.vue
@@ -1,5 +1,21 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <NuxtLayout>
+      <header>
+        <nav class="flex justify-between bg-gray-900 text-white w-screen">
+          <div class="px-2 xl:px-8 py-5 flex w-full items-center">
+            <ul class="hidden md:flex px-4 font-semibold font-heading space-x-6">
+              <li>
+                <NuxtLink to="/">Home</NuxtLink>
+              </li>
+              <li>
+                <NuxtLink to="/listOfStudents">List Of Students</NuxtLink>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
